a3232acbfe2b8b5abf408f3a187c6c380bed74e0
Eric Henderson <henderea@gmail.com>
2015-02-24 14:47:23 -0500

    use virtual size for MT memory stat

--- vendor/mem_info/mem_info.m
+++ vendor/mem_info/mem_info.m
@@ -165,7 +165,7 @@ get_percent_free(unsigned int* level)
     mach_msg_type_number_t size = sizeof(info);
     kern_return_t kerr = task_info(mach_task_self(), TASK_BASIC_INFO, (task_info_t)&info, &size);
     if( kerr == KERN_SUCCESS ) {
-        return info.resident_size;
+        return info.virtual_size;
     } else {
         NSLog(@"Error with task_info()");
         return -1;
