d6fa27915957b61a70808f1c54edf33f78265b84
Eric Henderson <henderea@gmail.com>
2015-11-02 13:33:15 -0500

    start copying over stuff from DO example

--- MTPrivilegedHelper/MTPrivilegedHelper/main.m
+++ MTPrivilegedHelper/MTPrivilegedHelper/main.m
@@ -7,80 +7,49 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <launch.h>
+#import <syslog.h>
 
-#include <syslog.h>
-#include <xpc/xpc.h>
+@interface PrivilegedObject : NSObject
 
-static void __XPC_Peer_Event_Handler(xpc_connection_t connection, xpc_object_t event) {
-    syslog(LOG_NOTICE, "Received event in helper.");
+- (int)purge;
 
-    xpc_type_t type = xpc_get_type(event);
+@end
 
-    if (type == XPC_TYPE_ERROR) {
-        if (event == XPC_ERROR_CONNECTION_INVALID) {
-            // The client process on the other end of the connection has either
-            // crashed or cancelled the connection. After receiving this error,
-            // the connection is in an invalid state, and you do not need to
-            // call xpc_connection_cancel(). Just tear down any associated state
-            // here.
+@implementation PrivilegedObject
 
-        } else if (event == XPC_ERROR_TERMINATION_IMMINENT) {
-            // Handle per-connection termination cleanup.
-        }
-
-    } else {
-        char *operation = (char *) xpc_dictionary_get_string(event, "operation");
-
-        char *responseMessage;
-
-        if(strcmp(operation, "purge") == 0) {
-            NSTask *purgeTask = [NSTask launchedTaskWithLaunchPath:@"/usr/sbin/purge" arguments:nil];
-            [purgeTask waitUntilExit];
-            [purgeTask release];
-            responseMessage = "purge complete";
-        } else {
-            responseMessage = "unknown operation";
-        }
-
-        xpc_connection_t remote = xpc_dictionary_get_remote_connection(event);
-
-        xpc_object_t reply = xpc_dictionary_create_reply(event);
-        xpc_dictionary_set_string(reply, "result", responseMessage);
-        xpc_connection_send_message(remote, reply);
-        xpc_release(reply);
-    }
+- (int)purge
+{
+    NSTask *task = [[NSTask alloc] init];
+    [task setLaunchPath: @"/usr/sbin/purge"];
+    [task launch];
 }
 
-static void __XPC_Connection_Handler(xpc_connection_t connection)  {
-    syslog(LOG_NOTICE, "Configuring message event handler for helper.");
+@end
 
-    xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
-        __XPC_Peer_Event_Handler(connection, event);
-    });
+int main (int argc, const char * argv[])
+{
+    @autoreleasepool {
+        
+        syslog(LOG_NOTICE, "MTPrivilegedHelper launched (uid: %d, euid: %d, pid: %d)", getuid(), geteuid(), getpid());
+        
+        launch_data_t req = launch_data_new_string(LAUNCH_KEY_CHECKIN);
+        launch_data_t resp = launch_msg(req);
+        launch_data_t machData = launch_data_dict_lookup(resp, LAUNCH_JOBKEY_MACHSERVICES);
+        launch_data_t machPortData = launch_data_dict_lookup(machData, "us.myepg.MemoryTamer.MTPrivilegedHelper.mach");
 
-    xpc_connection_resume(connection);
-}
+        mach_port_t mp = launch_data_get_machport(machPortData);
+        launch_data_free(req);
+        launch_data_free(resp);
 
-int main(int argc, const char *argv[]) {
-    xpc_connection_t service = xpc_connection_create_mach_service("us.myepg.MemoryTamer.MTPrivilegedHelper",
-                                                                  dispatch_get_main_queue(),
-                                                                  XPC_CONNECTION_MACH_SERVICE_LISTENER);
+        NSMachPort *rp = [[NSMachPort alloc] initWithMachPort:mp];
+        NSConnection *c = [NSConnection connectionWithReceivePort:rp sendPort:nil];
 
-    if (!service) {
-        syslog(LOG_NOTICE, "Failed to create service.");
-        exit(EXIT_FAILURE);
+        PrivilegedObject *obj = [PrivilegedObject new];
+        [c setRootObject:obj];
+        
+        [[NSRunLoop currentRunLoop] run];
+        
     }
-
-    syslog(LOG_NOTICE, "Configuring connection event handler for helper");
-    xpc_connection_set_event_handler(service, ^(xpc_object_t connection) {
-        __XPC_Connection_Handler(connection);
-    });
-
-    xpc_connection_resume(service);
-
-    dispatch_main();
-
-    xpc_release(service);
-
-    return EXIT_SUCCESS;
+    return 0;
 }
